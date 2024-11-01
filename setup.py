4f68a7bc5ebbfa9bf0c6e1714d24a5e1dde3346d
abhinesh-kourav <abhineshkourav@gmail.com>
2024-11-01 15:45:42 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="trading-bot",
+    version="0.0.1",
+    author="Abhinesh",
+    author_email="abhinesh.kourav@alumni.iitd.ac.in",
+    packages =find_packages(),
+    install_requires=[]
+)
